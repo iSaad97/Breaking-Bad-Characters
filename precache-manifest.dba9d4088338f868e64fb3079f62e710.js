4e909d4b8d79aeec6d7362fec09f22a30fe8f728
Saad Alessa <aleissa16@gmail.com>
2020-07-11 03:55:48 +0300

    Updates

---
+++ precache-manifest.dba9d4088338f868e64fb3079f62e710.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d530fe98fd9244a4e97e5b58ca05232e",
+    "url": "/Breaking-Bad-Characters/index.html"
+  },
+  {
+    "revision": "465a41100a7ae8eb8881",
+    "url": "/Breaking-Bad-Characters/static/css/main.ab07e98e.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/Breaking-Bad-Characters/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Breaking-Bad-Characters/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465a41100a7ae8eb8881",
+    "url": "/Breaking-Bad-Characters/static/js/main.61f10824.chunk.js"
+  },
+  {
+    "revision": "5590989e9ef048264160",
+    "url": "/Breaking-Bad-Characters/static/js/runtime-main.a7a5a484.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Characters/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Characters/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Characters/static/media/spinner.235b9b3e.gif"
+  }
+]);
